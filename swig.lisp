91ea03327ebe40ad237a6bb90d574ca769ee21fa
Sungjin Chun <chunsj@gmail.com>
2017-06-03 09:53:40 +0900

    indeinting...

--- swig.lisp
+++ swig.lisp
@@ -30,7 +30,7 @@
                       ((cl:lower-case-p c)
                        (helper (cl:cdr lst) 'lower (cl:cons (cl:char-upcase c) rest)))
                       ((cl:digit-char-p c)
-                       (helper (cl:cdr lst) 'digit 
+                       (helper (cl:cdr lst) 'digit
                                (cl:case last
                                  ((upper lower) (cl:list* c #\- rest))
                                  (cl:t (cl:cons c rest)))))
@@ -56,27 +56,27 @@
 (cffi:defcfun ("TF_Version" TF_Version) :string)
 
 (cffi:defcenum TF_DataType
-	(:TF_FLOAT #.1)
-	(:TF_DOUBLE #.2)
-	(:TF_INT32 #.3)
-	(:TF_UINT8 #.4)
-	(:TF_INT16 #.5)
-	(:TF_INT8 #.6)
-	(:TF_STRING #.7)
-	(:TF_COMPLEX64 #.8)
-	(:TF_COMPLEX #.8)
-	(:TF_INT64 #.9)
-	(:TF_BOOL #.10)
-	(:TF_QINT8 #.11)
-	(:TF_QUINT8 #.12)
-	(:TF_QINT32 #.13)
-	(:TF_BFLOAT16 #.14)
-	(:TF_QINT16 #.15)
-	(:TF_QUINT16 #.16)
-	(:TF_UINT16 #.17)
-	(:TF_COMPLEX128 #.18)
-	(:TF_HALF #.19)
-	(:TF_RESOURCE #.20))
+  (:TF_FLOAT #.1)
+  (:TF_DOUBLE #.2)
+  (:TF_INT32 #.3)
+  (:TF_UINT8 #.4)
+  (:TF_INT16 #.5)
+  (:TF_INT8 #.6)
+  (:TF_STRING #.7)
+  (:TF_COMPLEX64 #.8)
+  (:TF_COMPLEX #.8)
+  (:TF_INT64 #.9)
+  (:TF_BOOL #.10)
+  (:TF_QINT8 #.11)
+  (:TF_QUINT8 #.12)
+  (:TF_QINT32 #.13)
+  (:TF_BFLOAT16 #.14)
+  (:TF_QINT16 #.15)
+  (:TF_QUINT16 #.16)
+  (:TF_UINT16 #.17)
+  (:TF_COMPLEX128 #.18)
+  (:TF_HALF #.19)
+  (:TF_RESOURCE #.20))
 
 (cffi:defcfun ("TF_DataTypeSize" TF_DataTypeSize) :pointer
   (dt TF_DataType))
@@ -789,5 +789,3 @@
   (lib_handle :pointer))
 
 (cffi:defcfun ("TF_GetAllOpList" TF_GetAllOpList) :pointer)
-
-
